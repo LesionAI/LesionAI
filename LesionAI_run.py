e9f70a5d04241de39ab33de0edba6fe808ac3b66
LesionAI <lesionai@proton.me>
2025-04-22 21:12:42 +0200

    LesionAI_run.py

--- LesionAI_run.py
+++ LesionAI_run.py
@@ -30,4 +30,4 @@ if authentication_status:
 elif authentication_status is False:
     st.error("Incorrect username or password")
 elif authentication_status is None:
-    st.warning("Please enter your credentials")
+    st.info("👋 Happy to see you on **LesionAI**, the AI-powered assistant for intraoral lesion detection.")
